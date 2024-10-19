853e4cfbaa95c2df6aac724ddf11e6d5e937607a
Nadheer Chatharoo <nchatharoo@icloud.com>
2024-10-19 10:27:21 +0200

    Create a Calculator class

---
+++ SwiftUI_Calculator/Calculator.swift
@@ -0,0 +1,12 @@
+//
+//  Calculator.swift
+//  SwiftUI_Calculator
+//
+//  Created by Nadheer on 19/10/2024.
+//
+
+import Foundation
+
+class Calculator {
+    
+}
---
+++ SwiftUI_CalculatorTests/CalculatorTests.swift
@@ -0,0 +1,20 @@
+//
+//  CalculatorTests.swift
+//  CalculatorTests
+//
+//  Created by Nadheer on 19/10/2024.
+//
+
+import XCTest
+@testable import SwiftUI_Calculator
+
+final class CalculatorTests: XCTestCase {
+
+    var calculator: Calculator!
+    
+    override func setUp() {
+        super.setUp()
+        calculator = Calculator()
+    }
+
+}
