18156bfe5da6ccaa892ae5f318c3edcdfeed6511
Nadheer Chatharoo <nchatharoo@icloud.com>
2024-10-19 10:14:14 +0200

    Initial Commit

---
+++ SwiftUI_Calculator/SwiftUI_CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_CalculatorApp.swift
+//  SwiftUI_Calculator
+//
+//  Created by Nadheer on 19/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
